1e80093faac1999909275ac130d9f13127db9aa4
AndyEmilowa <andyowaa@gmail.com>
2023-12-04 11:20:20 +0200

    01

---
+++ WebShopApp/WebShopApp.Infrastructure/Data/Domain/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShopApp.Infrastructure.Data.Domain
+{
+    public class ApplicationUser
+    {
+        [Required]
+        [MaxLength(30)]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        [MaxLength(30)]
+        public string LastName { get; set; } = null!;
+        [Required]
+        [MaxLength(50)]
+        public string Address { get; set; } = null!;
+
+
+
+
+
+    }
+}
